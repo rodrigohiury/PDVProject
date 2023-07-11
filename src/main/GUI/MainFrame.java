d2d42d0d37ee4f6e527bd459d78f8790c2ef86e6
Miguel Maciel <miguelmaciel@alu.ufc.br>
2023-07-10 22:55:15 -0300

    Mais uma alteração

--- src/main/GUI/MainFrame.java
+++ src/main/GUI/MainFrame.java
@@ -58,7 +58,7 @@ public class MainFrame extends JFrame {
                 GUICadastrarProduto janela_1 = new GUICadastrarProduto(LookAndFeelSettings);
             }
         });
-        jBCadastrarProduto.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconCadastroProdutos.png")));
+        jBCadastrarProduto.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconCadastroProdutos.png")));
         jBCadastrarProduto.setBounds(36, 156, 201, 70);
         contentPane.add(jBCadastrarProduto);
 
@@ -68,7 +68,7 @@ public class MainFrame extends JFrame {
                 GUIAlterarPreco janela_2 = new GUIAlterarPreco(LookAndFeelSettings);
             }
         });
-        jBAlterarPreco.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconAlterarPreco.jpg")));
+        jBAlterarPreco.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconAlterarPreco.jpg")));
         jBAlterarPreco.setBounds(258, 65, 201, 70);
         contentPane.add(jBAlterarPreco);
 
@@ -78,7 +78,7 @@ public class MainFrame extends JFrame {
                 GUIRemoverProduto janela_3 = new GUIRemoverProduto(LookAndFeelSettings);
             }
         });
-        jBRemoverProduto.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconRemoverProduto.jpg")));
+        jBRemoverProduto.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconRemoverProduto.jpg")));
         jBRemoverProduto.setBounds(146, 246, 201, 70);
         contentPane.add(jBRemoverProduto);
 
@@ -88,7 +88,7 @@ public class MainFrame extends JFrame {
                 GUITableListagemProdutos janela_4 = new GUITableListagemProdutos(LookAndFeelSettings);
             }
         });
-        jBListarProdutos.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconListarProdutos.png")));
+        jBListarProdutos.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconListarProdutos.png")));
         jBListarProdutos.setBounds(258, 156, 201, 70);
         contentPane.add(jBListarProdutos);
 
@@ -98,7 +98,7 @@ public class MainFrame extends JFrame {
                 GUIConsultaProduto janela_5 = new GUIConsultaProduto(LookAndFeelSettings);
             }
         });
-        jBConsultarProdutos.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconConsultaProduto.jpg")));
+        jBConsultarProdutos.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconConsultaProduto.jpg")));
         jBConsultarProdutos.setBounds(36, 65, 201, 70);
         contentPane.add(jBConsultarProdutos);
 
@@ -114,12 +114,12 @@ public class MainFrame extends JFrame {
                 }
             }
         });
-        jBEstatisticas.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconEstatisticas.png")));
+        jBEstatisticas.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconEstatisticas.png")));
         jBEstatisticas.setBounds(506, 65, 201, 70);
         contentPane.add(jBEstatisticas);
 
         JButton jBCaixa = new JButton("Caixa");
-        jBCaixa.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconCaixa.png")));
+        jBCaixa.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconCaixa.png")));
         jBCaixa.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 CaixaGUI janela_7 = new CaixaGUI(LookAndFeelSettings);
@@ -190,7 +190,7 @@ public class MainFrame extends JFrame {
                 }
             }
         });
-        jBConfiguracoes.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconConfiguracao.jpg")));
+        jBConfiguracoes.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconConfiguracao.jpg")));
         jBConfiguracoes.setBounds(506, 341, 201, 70);
         contentPane.add(jBConfiguracoes);
 
@@ -203,7 +203,7 @@ public class MainFrame extends JFrame {
 
             }
         });
-        jBFuncionarios.setIcon(new ImageIcon(GUIgen.class.getResource("/Icons/IconFuncionario.png")));
+        jBFuncionarios.setIcon(new ImageIcon(MainFrame.class.getResource("/Icons/IconFuncionario.png")));
         jBFuncionarios.setBounds(506, 246, 201, 70);
         contentPane.add(jBFuncionarios);
 
